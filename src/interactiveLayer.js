4bd20df3682d37590425bce2a8a351f1889996a1
chethan27 <chethan.reddy2020@gmail.com>
2020-04-14 15:22:46 +0530

    added touchmove event handler to hide the tooltip when the user scrolls in mobile

--- src/interactiveLayer.js
+++ src/interactiveLayer.js
@@ -176,6 +176,12 @@ nv.interactiveGuideline = function() {
                 		pointXValue: pointXValue
                 	});
                 }
+                
+                // if user scrolls in mobile, hide the tooltip
+                if (d3.event.type === 'touchmove') {
+                    tooltip.hidden(true);
+                    return;
+                }
             }
 
             svgContainer
